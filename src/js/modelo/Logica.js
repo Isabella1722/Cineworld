6e243fdcb6af76f7fe1ca74aaf926b7dcc6c5e85
luisa moncayo <luisa_15092000@hotmail.com>
2019-11-14 17:59:08 -0500

    funciones agregadas

---
+++ src/js/modelo/Logica.js
@@ -0,0 +1,33 @@
+class Logica{
+    constructor(pantalla,listaPeliculas){
+        this.pantalla=pantalla;
+        this.listaPeliculas=listaPeliculas;
+    }
+
+
+    seleccionarCiudad() {
+        
+    }
+
+    seleccionarPelicula(){
+
+    }
+    generarBoleta(){
+
+    }
+    seleccionarSilla(){
+
+    }
+    mostrarCartelera(){
+
+    }
+    mostrarEstreno(){
+
+    }
+    calcularPrecio(){
+
+    }
+    pagarBoleta(){
+        
+    }
+}
