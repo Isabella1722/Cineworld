1955ee8d73328cbeb9d80ee7369a24516a030155
luisa moncayo <luisa_15092000@hotmail.com>
2019-11-14 17:41:38 -0500

    variables agregadas

---
+++ src/js/modelo/Ciudad.js
@@ -0,0 +1,5 @@
+class Ciudad{
+    constructor(nombre){
+        this.nombre=nombre;
+    }
+}
---
+++ src/js/modelo/Pelicula.js
@@ -0,0 +1,9 @@
+class Pelicula{
+    constructor(titulo,descripcion,duracion,formato,horario){
+        this.titulo=titulo;
+        this.descripcion=descripcion;
+        this.duracion=duracion;
+        this.formato=formato;
+        this.horario=horario;
+    }
+}
