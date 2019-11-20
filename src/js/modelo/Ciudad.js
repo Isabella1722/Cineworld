07964e6b11b26e6710b9291c81a12c7a4a21f7db
Isabella1722 <isabellalinarescamacho0517@gmail.com>
2019-11-19 23:10:07 -0500

    se crearon nuevas clases

--- src/js/modelo/Ciudad.js
+++ src/js/modelo/Ciudad.js
@@ -1,5 +1,10 @@
-class Ciudad{
-    constructor(nombre){
-        this.nombre=nombre;
-    }
-}
+let ciudad = document.querySelector(".ciudad");
+let medellin = document.querySelector(".medellin");
+let bogota = document.querySelector(".bogota");
+let ciudadElegida = document.querySelector(".ciudadElegida");
+
+
+
+let url = document.location.href;
+url = url.split("?")
+ciudad.textContent=url[1]
--- src/js/vista/PantallaInicioCiudad.js
+++ src/js/vista/PantallaInicioCiudad.js
@@ -1,9 +1,25 @@
-class PantallaInicioCiudad {
-    constructor() {
-        this.pantallaInicio;
-    }
 
-    dibujarPantalla() {
 
-    }
-}
+
+let contenedorModal = document.querySelector(".modalContenedor")
+let modal = document.querySelector(".modal")
+
+
+
+
+palabra.addEventListener("click", function () {
+    modal.classList.add("open")
+    contenedorModal.classList.add("open")
+
+})
+
+document.addEventListener("click", function (event) {
+   if(event.target==botonSalir){
+    modal.classList.remove("open")
+    contenedorModal.classList.remove("open")
+   }
+
+
+})
+
+
