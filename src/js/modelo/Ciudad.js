f1691219997bdbf8d2d6415a83e977b8f50f6dc6
Isabella1722 <isabellalinarescamacho0517@gmail.com>
2019-11-20 02:48:37 -0500

    se realizaron cambios

--- src/js/modelo/Ciudad.js
+++ src/js/modelo/Ciudad.js
@@ -1,4 +1,5 @@
 let ciudad = document.querySelector(".ciudad");
+let teatro = document.querySelector(".teatro");
 let medellin = document.querySelector(".medellin");
 let bogota = document.querySelector(".bogota");
 let ciudadElegida = document.querySelector(".ciudadElegida");
@@ -8,3 +9,4 @@ let ciudadElegida = document.querySelector(".ciudadElegida");
 let url = document.location.href;
 url = url.split("?")
 ciudad.textContent=url[1]
+teatro.textContent=url[2]
