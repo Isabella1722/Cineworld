e80d495870ff3e9550bcee7c7c6dd09668a8e995
luisa moncayo <luisa_15092000@hotmail.com>
2019-11-24 21:36:47 -0500

    info

--- src/js/vista/PantallaInformacion.js
+++ src/js/vista/PantallaInformacion.js
@@ -16,81 +16,124 @@ var peliculas = {
         "url": "elbromas.jpg"
     },
     1: {
-        "titulo": "chao",
-        "director": "Todd Philips",
-        "sinopsis": `Hola`,
+        "titulo": "Huerfanos de Brooklin",
+        "director": "Edward Norton",
+        "sinopsis": `Nueva York. Años 50.
+         Lionel Essrog es un solitario 
+         detective privado, afectado por 
+         el Síndrome de Tourette, que se 
+         aventura a intentar resolver el 
+         asesinato de su mentor y único
+          amigo, Frank Minna`,
         "url": "huerfanosdebrooklin.png"
     },
     2: {
-        "titulo": "sueño",
-        "director": "Todd Philips",
-        "sinopsis": `Hola`,
+        "titulo": "Doctor sueño",
+        "director": "Mike Flanagan",
+        "sinopsis": `Secuela del film de culto
+         "El resplandor" (1980) dirigido por 
+         Stanley Kubrick y también basado en 
+         una famosa novela de Stephen King. 
+         La historia transcurre algunos años 
+         después de los acontecimientos de 
+         "The Shining", y sigue a Danny Torrance 
+         (Ewan McGregor), traumatizado y con 
+         problemas de ira y alcoholismo que hacen
+          eco de los problemas de su padre Jack, 
+          que cuando sus habilidades psíquicas 
+          resurgen, se contacta con una niña de 
+          nombre Abra Stone, a quien debe rescatar 
+          de un grupo de viajeros que se alimentan 
+          de los niños que poseen el don de 
+          "el resplandor"`,
         "url": "doctor-sueno-.jpg"
     },
     3: {
-        "titulo": "pdg",
-        "director": "Todd Philips",
-        "sinopsis": `La pasión de Arthur Fleck,
-        un hombre ignorado por la
-        sociedad, es hacer reír a la
-        gente. Sin embargo, una
-        serie de trágicos sucesos 
-        harán que su visión del
-        mundo se distorsione
-        considerablemente
-        convirtiéndolo en un
-        brillante criminal.`,
+        "titulo": "Infierno en las alturas",
+        "director": "Tigran Sahakyan",
+        "sinopsis": `Cuando cinco amigos deciden celebrar Año Nuevo en una montaña, no saben que empiezan una experiencia terrorífica. En la subida al monte, quedan atrapados en el teleférico, sin esperanzas de rescate.  Ahora el miedo y el frío se apoderan de ellos y cada uno empieza a buscar la forma de sobrevivir sin que nada se interponga en su camino. Lo que comenzó como una fiesta, termina en una pesadilla en las alturas de la que no podrán escapar.`,
         "url": "infiernoenlasalturas.png"
     },
     4: {
-        "titulo": "losk",
-        "director": "Todd Philips",
-        "sinopsis": `Hola`,
+        "titulo": "Reflejos siniestros",
+        "director": "Aleksandr Domogarov",
+        "sinopsis": `En una antigua mansión, rodeada de macabros rumores, habita un espíritu en busca de nuevas víctimas. Cuando un grupo de amigos se pasea por esta zona prohibida, queda al descubierto un rito para invocarlo. El espíritu puede hacer realidad todos sus sueños, pero no se imaginan el precio que tendrán que pagar.`,
         "url": "reflejossiniestros.png"
     },
 
     5: {
-        "titulo": "keke",
-        "director": "Todd Philips",
-        "sinopsis": `Hola`,
-        "url": "reflejossiniestros.png"
+        "titulo": "Frozen 2",
+        "director": "Chris Buck, Jennifer Lee",
+        "sinopsis": `Del equipo ganador del Óscar®, los directores Jennifer Lee y Chris Buck y el productor Peter Del Vecho; con las voces originales en inglés de Idina Menzel, Kristen Bell, Jonathan Groff y Josh Gad; y la música de los compositores Kristen Anderson-Lopez y Robert Lopez, ganadores del premio de la Academia, llega FROZEN 2, la nueva película animada de Disney que estrena a partir del 21 de noviembre en cines de Colombia.`,
+        "url": "frozen.png"
     },
 
     6: {
-        "titulo": "losk",
-        "director": "Todd Philips",
-        "sinopsis": `Hola`,
-        "url": "reflejossiniestros.png"
+        "titulo": "Entre navajas y secretos",
+        "director": "Rian Johnson",
+        "sinopsis": `Cuando un reconocido novelista (Christopher Plummer) es encontrado muerto después de la celebración de su cumpleaños, un encantador detective Benoit Blanc (Daniel Craig) acepta el caso. Pero la investigación no será nada fácil: los sospechosos van desde la familia disfuncional del escritor hasta sus más devotos empleados. Blanc debe enfrentar un mundo de pistas, verdades y mentiras para descubrir la verdad detrás del misterio en ENTRE NAVAJAS Y SECRETOS: una historia divertida para toda la familia sobre un crimen en que todos son sospechosos. El aclamado escritor y director Rian Johnson (‘Asesino del Futuro’, ‘Star Wars Episodio VIII: Los Últimos Jedi’) rinde tributo a la mente maestra del misterio, Agatha Christie, de la mano de un elenco lleno de estrellas que incluye a Chris Evans, Ana de Armas, Jamie Lee Curtis, Don Johnson, Michael Shannon, Toni Collette y Katherine Langford.`,
+        "url": "entrenavajas.jpg"
     },
 
     7: {
-        "titulo": "losk",
-        "director": "Todd Philips",
-        "sinopsis": `Hola`,
-        "url": "reflejossiniestros.png"
+        "titulo": "Mascota jurasica",
+        "director": "Ryan Bellgardt",
+        "sinopsis": `Cuando un curioso niño descubre un pequeño dinosaurio, no solo encuentra a un nuevo amigo sino todo una aventura. Tendrá que hacer todo lo posible para salvar al dinosaurio de las garras de malvados científicos que quieren experimentar con él y ponerlo en libertad. Con la ayuda de sus amigos, buscará darle la libertad a su mascota jurásica`,
+        "url": "mascotajurasica.jpg"
     },
 
     8: {
-        "titulo": "losk",
-        "director": "Todd Philips",
-        "sinopsis": `Hola`,
-        "url": "reflejossiniestros.png"
+        "titulo": "Malefica",
+        "director": "Joachim Ronning",
+        "sinopsis": `En MALÉFICA: DUEÑA DEL MAL de Disney, la secuela del éxito de taquilla mundial de 2014, Maléfica y su ahijada Aurora comienzan a cuestionar los complejos lazos familiares que las unen, mientras son presionadas en diferentes direcciones por una boda inminente, aliados inesperados y nuevas fuerzas oscuras que entran en juego. `,
+        "url": "malefica.png"
     },
 
     9: {
-        "titulo": "losk",
-        "director": "Todd Philips",
-        "sinopsis": `Hola`,
-        "url": "reflejossiniestros.png"
+        "titulo": "Terminator",
+        "director": "Tim Miller",
+        "sinopsis": `Han pasado más de dos décadas desde que Sarah Connor evitó el Juicio Final, cambió el futuro y reescribió el destino de la raza humana. Dani Ramos (Natalia Reyes) vive de manera sencilla en México con su hermano (Diego Boneta) y su padre, cuando un nuevo Terminator, letal y tecnológicamente avanzado —un Rev-9 (Gabriel Luna)— viaja a través del tiempo para cazarla y matarla. La supervivencia de Dani dependerá de que sume fuerzas con dos guerreras: Grace (Mackenzie Davis), una mejorada súper-soldado del futuro, y una aguerrida Sarah Connor (Linda Hamilton). Mientras el Rev-9 destruye de forma despiadada todo y a todos los que se crucen en su camino para poder cazar a Dani, las tres serán guiadas hasta un T-800 (Arnold Schwarzenegger) perteneciente al pasado de Sarah, que bien podría ser su mejor y última esperanza.`,
+        "url": "terminator.jpg"
     },
     10: {
+        "titulo": "Los locos adams",
+        "director": "Conrad Vemon, Greg Tiernan",
+        "sinopsis": `¡Prepárate para chasquear los dedos! La familia Addams está de vuelta en la pantalla grande, en la primera comedia animada sobre la familia más atrevida del barrio. Divertida, extravagante y completamente icónica, la familia Addams redefine lo que significa ser un buen vecino.`,
+        "url": "loslocosaddams.jpg"
+    },
+
+    11: {
         "titulo": "losk",
         "director": "Todd Philips",
         "sinopsis": `Hola`,
         "url": "reflejossiniestros.png"
     },
 
-    11: {
+    12: {
+        "titulo": "losk",
+        "director": "Todd Philips",
+        "sinopsis": `Hola`,
+        "url": "reflejossiniestros.png"
+    },
+    13: {
+        "titulo": "losk",
+        "director": "Todd Philips",
+        "sinopsis": `Hola`,
+        "url": "reflejossiniestros.png"
+    },
+    14: {
+        "titulo": "losk",
+        "director": "Todd Philips",
+        "sinopsis": `Hola`,
+        "url": "reflejossiniestros.png"
+    },
+    15: {
+        "titulo": "losk",
+        "director": "Todd Philips",
+        "sinopsis": `Hola`,
+        "url": "reflejossiniestros.png"
+    },
+    16: {
         "titulo": "losk",
         "director": "Todd Philips",
         "sinopsis": `Hola`,
